c612fee3808f932e39fe0c635a1fea962549d018
marcelogeraldo11 <m.geraldo11.11@gmail.com>
2023-10-20 21:25:29 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+
+
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const countLabel = document.getElementById("countLabel");
+let count = 0;
+
+
+increaseBtn.onclick = function(){
+    count++;
+    countLabel.textContent = count;
+}
+decreaseBtn.onclick = function(){
+    count--;
+    countLabel.textContent = count;
+}
+
+resetBtn.onclick = function(){
+    count = 0;
+    countLabel.textContent = count;
+
+
+}
+
+const modeToggleButton = document.getElementById("modeToggleButton");
+
+modeToggleButton.addEventListener("click", toggleDarkMode);
+
+function toggleDarkMode() {
+    const body = document.body;
+    const darkModeStyles = document.getElementById("darkModeStyles");
+
+    body.classList.toggle("dark-mode");
+    darkModeStyles.disabled = !darkModeStyles.disabled;
+}
